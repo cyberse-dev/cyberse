b4c598a1ae0c0d72414792b50457e1600620fd75
Mark Peter Fejes <fejes.mark@gmail.com>
2019-10-04 23:00:25 +0200

    style: reformatted code with new prettier config

--- apps/core-e2e/src/integration/app.spec.ts
+++ apps/core-e2e/src/integration/app.spec.ts
@@ -1,9 +1,9 @@
 import { getGreeting } from '../support/app.po';
 
 describe('core', () => {
-  beforeEach(() => cy.visit('/'));
+    beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to core!');
-  });
+    it('should display welcome message', () => {
+        getGreeting().contains('Welcome to core!');
+    });
 });
